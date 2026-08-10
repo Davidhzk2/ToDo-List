199c6c9b9f350493d7e48f35d29476183c912568
Davidhzk2 <guffyherrera150@gmail.com>
2026-08-10 12:25:38 -0500

    alert para editar la modal y acciones de eliminar

--- src/app/components/category-modal/category-modal.component.ts
+++ src/app/components/category-modal/category-modal.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import{FormsModule} from '@angular/forms';
-import {ModalController } from '@ionic/angular';
+import {ModalController, AlertController } from '@ionic/angular';
 import {IonContent, IonTitle, IonHeader, IonIcon, IonToolbar, IonButtons, IonButton, IonItem, IonInput, IonLabel, IonList} from '@ionic/angular/standalone';
 import { Category } from 'src/app/core/models/todo.model';
 
@@ -16,15 +16,57 @@ export class CategoryModalComponent implements OnInit {
   public newCategoryName: string = '';
   public categories: Category[] = [];
 
-  constructor(private modalCtrl: ModalController) { }
+  constructor(private modalCtrl: ModalController, private alertCtrl: AlertController) { }
 
   ngOnInit() {}
 
   addCategory() {
     if (this.newCategoryName.trim()) {
       this.categories.push({ id: Date.now().toString(), name: this.newCategoryName.trim() });
+      localStorage.setItem('categories', JSON.stringify(this.categories));
       this.newCategoryName = '';
     }
+  }
+
+  async editCategory(category: Category) {
+    const alert = this.alertCtrl.create({
+      header: 'Editar Categoría',
+      inputs: [
+        {
+          name: 'categoryName',
+          type: 'text',
+          value: category.name, 
+          placeholder: 'Nombre de la categoría'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar', role: 'cancel'
+        },
+        {
+          text: 'Guardar',
+          handler: (data) => {
+            if (data.categoryName.trim()) {
+              category.name = data.categoryName.trim();
+              localStorage.setItem('categories', JSON.stringify(this.categories));
+            }
+          }
+        }
+      ]
+    });
+    await alert.then(alertEl => alertEl.present());
+  }
+
+  deleteCategory(category: Category) {
+    const index = this.categories.indexOf(category);
+    if (index > -1) {
+      this.categories.splice(index, 1);
+    }
+  }
+
+  saveCategories() {
+    localStorage.setItem('categories', JSON.stringify(this.categories));
+    this.modalCtrl.dismiss();
   }
 
   dismiss(){
